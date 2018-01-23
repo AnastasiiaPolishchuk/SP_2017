06554ac9505c22e4c62ce703276b15b19a4b85c3
Christian Mösl <christian.moesl@live.at>
2018-01-23 21:44:18 +0100

    Added uncaught exception handler

--- app/src/main/java/com/annapol04/munchkin/gui/MainActivity.java
+++ app/src/main/java/com/annapol04/munchkin/gui/MainActivity.java
@@ -18,6 +18,5 @@ public class MainActivity extends AppCompatActivity {
 
     public void start(View view) {
         startActivity(new Intent(this, PlayDeskActivity.class));
-        // NewPlayDesk <--> PlayDeskActivity
     }
 }
