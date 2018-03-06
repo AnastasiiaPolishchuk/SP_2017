70ca332d10ed96eaff0f030db656f7e2b58d2e62
Falco Goedde <s1019619@stud.sbg.ac.at>
2018-03-06 13:26:03 +0100

    Corrected the drawable names in DoorCards.java & TreasureCards.java

--- app/src/main/java/com/annapol04/munchkin/engine/DoorCards.java
+++ app/src/main/java/com/annapol04/munchkin/engine/DoorCards.java
@@ -9,12 +9,12 @@ public class DoorCards {
     public static final Card LAME_GOBLIN    = new Monster(R.string.lame_goblin, R.string.lame_goblin_desc, R.drawable.plain, 1, 1, "bad Things");
     public static final Card POTTED_PLANT   = new Monster(R.string.potted_plant, R.string.potted_plant_desc, R.drawable.plain, 1, 1, "bad Things");
     public static final Card FLYING_FROGS   = new Monster(R.string.flying_frogs, R.string.flying_frogs_desc, R.drawable.plain, 2, 1, "bad Things");
-    public static final Card GALLERTOKTAEDER= new Monster(R.string.gallertoktaeder, R.string.gallertoktaeder_desc, R.drawable.octahedron, 1, 1, "bad Things");
+    public static final Card GALLERTOKTAEDER= new Monster(R.string.gallertoktaeder, R.string.gallertoktaeder_desc, R.drawable.gelatinous_octaedron, 1, 1, "bad Things");
     public static final Card CHICKEN        = new Monster(R.string.chicken, R.string.chicken_desc, R.drawable.plain, 1, 1, "bad Things");
 
     public static final Card MR_BONES = new Monster(R.string.mr_bones, R.string.mr_bones_desc, R.drawable.plain, 2, 1, "bad Things");
     public static final Card MAUL_RAT = new Monster(R.string.maul_rat, R.string.maul_rat_desc, R.drawable.plain, 1, 1, "bad Things");
-    public static final Card OCTAHEDRON = new Monster(R.string.octahedron, R.string.octahedron_desc, R.drawable.octahedron, 2, 1, "bad Things");
+    public static final Card OCTAHEDRON = new Monster(R.string.octahedron, R.string.octahedron_desc, R.drawable.gelatinous_octaedron, 2, 1, "bad Things");
     public static final Card CRABS      = new Monster(R.string.crabs, R.string.crabs_desc, R.drawable.crabs, 1, 1, "bad Things");
     public static final Card FACE_SUCKER = new Monster(R.string.face_sucker, R.string.face_sucker_desc, R.drawable.plain, 8, 2, "bad Things");
     public static final Card HARPIES = new Monster(R.string.harpies, R.string.harpies_desc, R.drawable.plain, 4, 2, "bad Things");
--- app/src/main/java/com/annapol04/munchkin/engine/TreasureCards.java
+++ app/src/main/java/com/annapol04/munchkin/engine/TreasureCards.java
@@ -4,29 +4,29 @@ import com.annapol04.munchkin.R;
 
 public class TreasureCards {
     public static final Card ELEVEN_FOOT_POLE      = new BonusWear(R.string.pole, R.string.pole_desc, R.drawable.plain,1, BonusWear.Blocking.TWOHANDS, BonusWear.Size.SMALL);
-    public static final Card HELM_OF_COURAGE       = new BonusWear(R.string.helmet, R.string.helmet_desc, R.drawable.helmofcourage, 1, BonusWear.Blocking.HEAD, BonusWear.Size.SMALL);
-    public static final Card LEATHER_ARMOR         = new BonusWear(R.string.boiled_leather, R.string.boiled_leather_desc, R.drawable.tleatherarmor, 1, BonusWear.Blocking.ARMOR, BonusWear.Size.SMALL);
+    public static final Card HELM_OF_COURAGE       = new BonusWear(R.string.helmet, R.string.helmet_desc, R.drawable.helm_of_courage, 1, BonusWear.Blocking.HEAD, BonusWear.Size.SMALL);
+    public static final Card LEATHER_ARMOR         = new BonusWear(R.string.boiled_leather, R.string.boiled_leather_desc, R.drawable.leather_armor, 1, BonusWear.Blocking.ARMOR, BonusWear.Size.SMALL);
     public static final Card SLIMY_ARMOR          = new BonusWear(R.string.slimy_armor, R.string.slimy_armor_desc, R.drawable.plain, 1, BonusWear.Blocking.ARMOR, BonusWear.Size.SMALL);
-    public static final Card SPIKY_KNEES           = new BonusWear(R.string.knee, R.string.knee_desc, R.drawable.tspikyknies, 1, BonusWear.Blocking.NOTHING, BonusWear.Size.SMALL);
-    public static final Card HORNY_HELMET          = new BonusWear(R.string.horny_helmet, R.string.horny_helmet_desc, R.drawable.thornyhelmet, 1, BonusWear.Blocking.HEAD, BonusWear.Size.SMALL);
+    public static final Card SPIKY_KNEES           = new BonusWear(R.string.knee, R.string.knee_desc, R.drawable.spiky_knees, 1, BonusWear.Blocking.NOTHING, BonusWear.Size.SMALL);
+    public static final Card HORNY_HELMET          = new BonusWear(R.string.horny_helmet, R.string.horny_helmet_desc, R.drawable.thorny_helmet, 1, BonusWear.Blocking.HEAD, BonusWear.Size.SMALL);
     public static final Card BOOTS_OF_BUTT_KICKING = new BonusWear(R.string.ass_kick_boot, R.string.ass_kick_boot_desc, R.drawable.plain, 2, BonusWear.Blocking.SHOES, BonusWear.Size.SMALL);
     public static final Card DAGGER_OF_TREACHERY   = new BonusWear(R.string.dagger_of_treachery, R.string.dagger_of_treachery_desc, R.drawable.plain,3, BonusWear.Blocking.ONEHAND, BonusWear.Size.SMALL);
     public static final Card RAPIER_OF_UNFAIRNESS  = new BonusWear(R.string.rapier_of_unfairness, R.string.rapier_of_unfairness_desc, R.drawable.plain,3, BonusWear.Blocking.ONEHAND, BonusWear.Size.SMALL);
     public static final Card REALLY_IMPRESSIVE_TITLE   = new BonusWear(R.string.really_impressive_title, R.string.really_impressive_title_desc, R.drawable.plain,3, BonusWear.Blocking.NOTHING, BonusWear.Size.SMALL);
     public static final Card SWISS_ARMY_POLEARM    = new BonusWear(R.string.swiss_army_polearm, R.string.swiss_army_polearm_desc, R.drawable.plain,4, BonusWear.Blocking.TWOHANDS, BonusWear.Size.BIG);
     public static final Card SINGING_DANCING_SWORD = new BonusWear(R.string.singing_dancing_sword, R.string.singing_dancing_sword_desc, R.drawable.plain,2, BonusWear.Blocking.NOTHING, BonusWear.Size.SMALL);
-    public static final Card SANDALS_OF_PROTECTION = new BonusWear(R.string.sandals_protection, R.string.sandals_protection_desc, R.drawable.tsandalsofprotektion,0, BonusWear.Blocking.SHOES, BonusWear.Size.SMALL);
-    public static final Card RAT_ON_STICK          = new BonusWear(R.string.rat, R.string.rat_desc, R.drawable.tratonastick,1, BonusWear.Blocking.ONEHAND, BonusWear.Size.SMALL);
-    public static final Card POINTY_HAT_OF_POWER   = new BonusWear(R.string.pointy_hat, R.string.pointy_hat_desc, R.drawable.tpointyhatofpower,3, BonusWear.Blocking.HEAD, BonusWear.Size.SMALL);
+    public static final Card SANDALS_OF_PROTECTION = new BonusWear(R.string.sandals_protection, R.string.sandals_protection_desc, R.drawable.sandals_of_protection,0, BonusWear.Blocking.SHOES, BonusWear.Size.SMALL);
+    public static final Card RAT_ON_STICK          = new BonusWear(R.string.rat, R.string.rat_desc, R.drawable.rat_on_a_stick,1, BonusWear.Blocking.ONEHAND, BonusWear.Size.SMALL);
+    public static final Card POINTY_HAT_OF_POWER   = new BonusWear(R.string.pointy_hat, R.string.pointy_hat_desc, R.drawable.pointy_hat_of_power,3, BonusWear.Blocking.HEAD, BonusWear.Size.SMALL);
     public static final Card BOOTS_OF_RUNNING_RF   = new BonusWear(R.string.boots_of_running, R.string.boots_of_running_desc, R.drawable.plain,0, BonusWear.Blocking.SHOES, BonusWear.Size.SMALL);
-    public static final Card STAFF_OF_NAPALM      = new BonusWear(R.string.staff_napalm, R.string.staff_napalm_desc, R.drawable.tstaffofnapalm,5, BonusWear.Blocking.ONEHAND, BonusWear.Size.SMALL);
-    public static final Card SNEAKY_BASTARD_SWORD = new BonusWear(R.string.sneaky_sword, R.string.sneaky_sword_desc, R.drawable.tsneakybastardsword,2, BonusWear.Blocking.ONEHAND, BonusWear.Size.SMALL);
-    public static final Card BROAD_SWORD          = new BonusWear(R.string.broad_sword, R.string.broad_sword_desc, R.drawable.tbroadsword,3, BonusWear.Blocking.ONEHAND, BonusWear.Size.SMALL);
-    public static final Card TUBA_OF_CHARM        = new BonusWear(R.string.tuba_of_charm, R.string.tuba_of_charm_desc, R.drawable.ttubaofcharm,0, BonusWear.Blocking.ONEHAND, BonusWear.Size.BIG);
+    public static final Card STAFF_OF_NAPALM      = new BonusWear(R.string.staff_napalm, R.string.staff_napalm_desc, R.drawable.staff_of_napalm,5, BonusWear.Blocking.ONEHAND, BonusWear.Size.SMALL);
+    public static final Card SNEAKY_BASTARD_SWORD = new BonusWear(R.string.sneaky_sword, R.string.sneaky_sword_desc, R.drawable.sneaky_bastard_sword,2, BonusWear.Blocking.ONEHAND, BonusWear.Size.SMALL);
+    public static final Card BROAD_SWORD          = new BonusWear(R.string.broad_sword, R.string.broad_sword_desc, R.drawable.broad_sword,3, BonusWear.Blocking.ONEHAND, BonusWear.Size.SMALL);
+    public static final Card TUBA_OF_CHARM        = new BonusWear(R.string.tuba_of_charm, R.string.tuba_of_charm_desc, R.drawable.tuba_of_charm,0, BonusWear.Blocking.ONEHAND, BonusWear.Size.BIG);
 
-    public static final Card HUGE_ROCK        = new BonusWear(R.string.huge_rock, R.string.huge_rock_desc, R.drawable.thugerock,3, BonusWear.Blocking.TWOHANDS, BonusWear.Size.BIG);
+    public static final Card HUGE_ROCK        = new BonusWear(R.string.huge_rock, R.string.huge_rock_desc, R.drawable.huge_rock,3, BonusWear.Blocking.TWOHANDS, BonusWear.Size.BIG);
     public static final Card LIMBURGER_SANDWICH   = new BonusWear(R.string.limburger_sandwich, R.string.limburger_sandwich_desc, R.drawable.plain,3, BonusWear.Blocking.NOTHING, BonusWear.Size.SMALL);
-    public static final Card MITHRIL_ARMOR        = new BonusWear(R.string.mithril_armor, R.string.mithril_armor_desc, R.drawable.tmithrilarmor,3, BonusWear.Blocking.ARMOR, BonusWear.Size.BIG);
+    public static final Card MITHRIL_ARMOR        = new BonusWear(R.string.mithril_armor, R.string.mithril_armor_desc, R.drawable.mithril_armor,3, BonusWear.Blocking.ARMOR, BonusWear.Size.BIG);
     public static final Card STEPLADDER        = new BonusWear(R.string.stepladder, R.string.stepladder_desc, R.drawable.plain,3, BonusWear.Blocking.NOTHING, BonusWear.Size.BIG);
 
 }
