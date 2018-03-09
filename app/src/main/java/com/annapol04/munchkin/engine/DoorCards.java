d70546daf3271db4961a068f9e039169398665e8
Falco Goedde <s1019619@stud.sbg.ac.at>
2018-03-09 19:05:16 +0100

    Fixed card miss

--- app/src/main/java/com/annapol04/munchkin/engine/DoorCards.java
+++ app/src/main/java/com/annapol04/munchkin/engine/DoorCards.java
@@ -5,20 +5,20 @@ import com.annapol04.munchkin.R;
 public class DoorCards {
     //Monster Cards: 37
     public static final Card LEPERCHAUN     = new Monster(R.string.leperchaun, R.string.leperchaun_desc, R.drawable.leperchaun, 4, 2, "bad Things");
-    public static final Card DROOLING_SLIME= new Monster(R.string.drooling_slime, R.string.sabbering_slime_desc, R.drawable.plain, 1, 1, "bad Things");
-    public static final Card LAME_GOBLIN    = new Monster(R.string.lame_goblin, R.string.lame_goblin_desc, R.drawable.plain, 1, 1, "bad Things");
-    public static final Card POTTED_PLANT   = new Monster(R.string.potted_plant, R.string.potted_plant_desc, R.drawable.plain, 1, 1, "bad Things");
-    public static final Card FLYING_FROGS   = new Monster(R.string.flying_frogs, R.string.flying_frogs_desc, R.drawable.plain, 2, 1, "bad Things");
+    public static final Card DROOLING_SLIME= new Monster(R.string.drooling_slime, R.string.drooling_slime_desc, R.drawable.drooling_slime, 1, 1, "bad Things");
+    public static final Card LAME_GOBLIN    = new Monster(R.string.lame_goblin, R.string.lame_goblin_desc, R.drawable.lame_goblin, 1, 1, "bad Things");
+    public static final Card POTTED_PLANT   = new Monster(R.string.potted_plant, R.string.potted_plant_desc, R.drawable.potted_plant, 1, 1, "bad Things");
+    public static final Card FLYING_FROGS   = new Monster(R.string.flying_frogs, R.string.flying_frogs_desc, R.drawable.flying_frogs, 2, 1, "bad Things");
     public static final Card GALLERTOKTAEDER= new Monster(R.string.gallertoktaeder, R.string.gallertoktaeder_desc, R.drawable.gelatinous_octaedron, 1, 1, "bad Things");
     public static final Card LARGE_ANGRY_CHICKEN        = new Monster(R.string.large_angry_chicken, R.string.large_angry_chicken_desc, R.drawable.large_angry_chicken, 1, 1, "bad Things");
 
-    public static final Card MR_BONES = new Monster(R.string.mr_bones, R.string.mr_bones_desc, R.drawable.plain, 2, 1, "bad Things");
-    public static final Card MAUL_RAT = new Monster(R.string.maul_rat, R.string.maul_rat_desc, R.drawable.plain, 1, 1, "bad Things");
-    public static final Card OCTAHEDRON = new Monster(R.string.octahedron, R.string.octahedron_desc, R.drawable.gelatinous_octaedron, 2, 1, "bad Things");
+    public static final Card MR_BONES = new Monster(R.string.mr_bones, R.string.mr_bones_desc, R.drawable.mr_bones, 2, 1, "bad Things");
+    public static final Card MAUL_RAT = new Monster(R.string.maul_rat, R.string.maul_rat_desc, R.drawable.maul_rat, 1, 1, "bad Things");
+    //public static final Card OCTAHEDRON = new Monster(R.string.octahedron, R.string.octahedron_desc, R.drawable.gelatinous_octaedron, 2, 1, "bad Things");
     public static final Card CRABS      = new Monster(R.string.crabs, R.string.crabs_desc, R.drawable.crabs, 1, 1, "bad Things");
-    public static final Card FACE_SUCKER = new Monster(R.string.face_sucker, R.string.face_sucker_desc, R.drawable.plain, 8, 2, "bad Things");
-    public static final Card HARPIES = new Monster(R.string.harpies, R.string.harpies_desc, R.drawable.plain, 4, 2, "bad Things");
-    public static final Card UNDEAD_HORSE = new Monster(R.string.undead_horse, R.string.undead_horse_desc, R.drawable.plain, 4, 2, "bad Things");
+    public static final Card FACE_SUCKER = new Monster(R.string.face_sucker, R.string.face_sucker_desc, R.drawable.face_sucker, 8, 2, "bad Things");
+    public static final Card HARPIES = new Monster(R.string.harpies, R.string.harpies_desc, R.drawable.harpies, 4, 2, "bad Things");
+    public static final Card UNDEAD_HORSE = new Monster(R.string.undead_horse, R.string.undead_horse_desc, R.drawable.undead_horse, 4, 2, "bad Things");
 
     //New
     public static final Card AMAZON         = new Monster(R.string.amazon, R.string.amazon_desc, R.drawable.amazon, 8, 2, "bad Things");
